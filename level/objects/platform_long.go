f994cf2af8d8aafdca6e460b243bba6d27f4996a
Eng. Carlos E F Correa <kadusjc1984@gmail.com>
2020-05-08 11:06:12 -0300

    Adding spikes to plataform long either

--- level/objects/platform_long.go
+++ level/objects/platform_long.go
@@ -100,3 +100,267 @@ embedded_components {
     w: 6.123234E-17
   }
 }
+embedded_components {
+  id: "danger_shapes"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"danger\"\n"
+  "mask: \"hero\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: -199.0\n"
+  "      y: -1.0\n"
+  "      z: 1.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 575.0\n"
+  "      y: 4.0\n"
+  "      z: 1.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 3\n"
+  "    count: 3\n"
+  "  }\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: -77.0\n"
+  "      z: 1.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: -0.7075842\n"
+  "      w: 0.70662904\n"
+  "    }\n"
+  "    index: 6\n"
+  "    count: 3\n"
+  "  }\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 380.0\n"
+  "      y: -77.0\n"
+  "      z: 1.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: -0.7075842\n"
+  "      w: 0.70662904\n"
+  "    }\n"
+  "    index: 9\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 22.855\n"
+  "  data: 52.9435\n"
+  "  data: 9.8\n"
+  "  data: 22.855\n"
+  "  data: 52.9435\n"
+  "  data: 9.8\n"
+  "  data: 22.855\n"
+  "  data: 165.494\n"
+  "  data: 9.8\n"
+  "  data: 22.855\n"
+  "  data: 165.494\n"
+  "  data: 9.8\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite2"
+  type: "sprite"
+  data: "tile_set: \"/level/level.atlas\"\n"
+  "default_animation: \"spikes\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: -197.0
+    y: -3.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite3"
+  type: "sprite"
+  data: "tile_set: \"/level/level.atlas\"\n"
+  "default_animation: \"spikes\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 575.0
+    y: 3.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: -0.99999934
+    w: 0.0011224704
+  }
+}
+embedded_components {
+  id: "sprite4"
+  type: "sprite"
+  data: "tile_set: \"/level/level.atlas\"\n"
+  "default_animation: \"spikes\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: -115.0
+    y: -82.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.70710677
+    w: 0.70710677
+  }
+}
+embedded_components {
+  id: "sprite5"
+  type: "sprite"
+  data: "tile_set: \"/level/level.atlas\"\n"
+  "default_animation: \"spikes\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 13.0
+    y: -82.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.70710677
+    w: 0.70710677
+  }
+}
+embedded_components {
+  id: "sprite6"
+  type: "sprite"
+  data: "tile_set: \"/level/level.atlas\"\n"
+  "default_animation: \"spikes\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 109.0
+    y: -80.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.70710677
+    w: 0.70710677
+  }
+}
+embedded_components {
+  id: "sprite7"
+  type: "sprite"
+  data: "tile_set: \"/level/level.atlas\"\n"
+  "default_animation: \"spikes\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 270.0
+    y: -82.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.70710677
+    w: 0.70710677
+  }
+}
+embedded_components {
+  id: "sprite8"
+  type: "sprite"
+  data: "tile_set: \"/level/level.atlas\"\n"
+  "default_animation: \"spikes\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 403.0
+    y: -82.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.70710677
+    w: 0.70710677
+  }
+}
+embedded_components {
+  id: "sprite9"
+  type: "sprite"
+  data: "tile_set: \"/level/level.atlas\"\n"
+  "default_animation: \"spikes\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 500.0
+    y: -80.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.70710677
+    w: 0.70710677
+  }
+}
